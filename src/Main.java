e014fbdcb8f0907c4ddafef465e5b32d998693ab
Karl Engwall <karlengwall@gmail.com>
2023-12-11 11:23:47 +0100

    Moved Functions to class Banksystem and removed classes with moved functions, changed variable and method names to english.

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LoggaIn l = new LoggaIn();
-        bliMedlem b = new bliMedlem();
-        String personnummer;
-        String namn;
-        String Alternativ;
+        Banksystem banksystem = new Banksystem();
 
         while(true){
             System.out.print("Skriv 1 för att logga in eller 2 för att registerar dig: ");
             Scanner scan = new Scanner(System.in);
-            Alternativ = scan.next();
-            if(Alternativ.equals("1")){
-                System.out.print("personnummer: ");
-                Scanner scan2 = new Scanner(System.in);
-                String personnummer1 = scan2.nextLine();
-                System.out.print("namn: ");
-                String namn1 = scan2.nextLine();
-                l.loggaIn(personnummer1, namn1);
-            }else if(Alternativ.equals("2")){
-                System.out.print("personnummer: ");
-                Scanner scan3 = new Scanner(System.in);
-                personnummer = scan3.nextLine();
-                System.out.print("namn: ");
-                namn = scan3.nextLine();
-                b.saveMembers(personnummer, namn);
+            String choice = scan.next();
+
+            if (choice.equals("1")){
+                banksystem.logIn();
+            } else if (choice.equals("2")) {
+                banksystem.registerMember();
+            } else {
+                System.out.println("Ogiltigt val. Försök igen");
             }
         }
 
